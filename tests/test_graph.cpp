a1a67c1a88e6812a8737b14e81f0096ad6d9c481
Elie Nassar <nassarelie98@gmail.com>
2026-05-21 15:48:49 +0200

    More tests for graphs

--- tests/test_graph.cpp
+++ tests/test_graph.cpp
@@ -8,6 +8,18 @@ TEST_CASE("graph default constructor") {
     CHECK(g.get_nodes().empty());
 }
 
+TEST_CASE("directed graph") {
+    graph g(true);
+    CHECK(g.get_nodes().empty());
+    g.add_node();
+    g.add_node();
+    CHECK(g.get_nodes().size()==2);
+    g.add_edge(0,1);
+    CHECK(g.get_node_by_id(0).get_neighbor_ids().size()==1);
+    CHECK(g.get_node_by_id(1).get_neighbor_ids().size()==0);
+    CHECK(g.is_well_formed());
+}
+
 TEST_CASE("add_node") {
     graph g;
     int id0 = g.add_node();
@@ -17,6 +29,7 @@ TEST_CASE("add_node") {
     CHECK(id1==1);
     CHECK(g.get_node_by_id(id0).get_id()==0);
     CHECK(g.get_node_by_id(id1).get_id()==1);
+    CHECK(g.is_well_formed());
 }
 
 TEST_CASE("add_edge") {
@@ -32,4 +45,12 @@ TEST_CASE("add_edge") {
     CHECK(g.get_node_by_id(id2).get_neighbor_ids().contains(id0));
     CHECK(!g.get_node_by_id(id1).get_neighbor_ids().contains(id2));
     CHECK(!g.get_node_by_id(id2).get_neighbor_ids().contains(id1));
+    CHECK(g.is_well_formed());
+}
+
+TEST_CASE("random") {
+    for(int i=2;i<10;i++) {
+        graph g = graph::random(i,i*2);
+        CHECK(g.is_well_formed());
+    }
 }
