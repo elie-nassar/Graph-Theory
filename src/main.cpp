4bd2ee4db5f4b9af2126270e676655933b478c8a
Elie Nassar <nassarelie98@gmail.com>
2026-05-25 16:48:24 +0200

    SAT/Backtracking comparison

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,15 @@ int main() {
     srand(time(0));
     
 
-    float tot = 0;
+    float tot_back = 0, tot_sat = 0;
     for(int i=0;i<30;i++) {
-        graph G = graph::random(100,150);
+        graph G = graph::random(50,250);
         //G.save("img");
 
         clock_t before = clock();
-        //unordered_map<int,int> c_back = proper_coloring_backtracking(G,3);
-        //clock_t duration_back = clock() - before;
-        //cout << "Backtracking : " << (float)duration_back / CLOCKS_PER_SEC << "sec" << endl;
+        unordered_map<int,int> c_back = proper_coloring_backtracking(G,3);
+        clock_t duration_back = clock() - before;
+        cout << "Backtracking : " << (float)duration_back / CLOCKS_PER_SEC << "sec" << endl;
         before = clock();
         unordered_map<int,int> c_sat = proper_coloring_sat(G,3);
         clock_t duration_sat = clock() - before;
@@ -26,14 +26,16 @@ int main() {
         //unordered_map<int,int> c_dp = proper_coloring_dp(G,3);
         //clock_t duration_dp = clock() - before;
         //cout << "DP : " << (float)duration_dp / CLOCKS_PER_SEC << "sec" << endl;
-        //if(!verify_proper_coloring(G,3,c_back)) cout << "ERREUR BACKTRACKING" << endl;
+        if(!verify_proper_coloring(G,3,c_back)) cout << "ERREUR BACKTRACKING" << endl;
         if(!verify_proper_coloring(G,3,c_sat)) cout << "ERREUR SAT" << endl;
         //if(!verify_proper_coloring(G,3,c_dp)) cout << "ERREUR DP" << endl;
         //G.save("coloring_backtracking",c_back);
         //G.save("coloring_sat",c_sat);
-        tot+=(float)duration_sat / CLOCKS_PER_SEC;
+        tot_back+=(float)duration_back / CLOCKS_PER_SEC;
+        tot_sat+=(float)duration_sat / CLOCKS_PER_SEC;
     }
-    cout << "Moyenne : " << tot/100 << "sec" << endl;
+    cout << "Moyenne Backtracking : " << tot_back/100 << "sec" << endl;
+    cout << "Moyenne SAT : " << tot_sat/100 << "sec" << endl;
 
     /* sat SAT = sat({
         {1,2,3},
