9d2e28830dd6e81d534b9682435a8865df10b9a1
Elie Nassar <nassarelie98@gmail.com>
2026-05-31 22:31:41 +0200

    Dominator coloring backtracking

--- src/coloring/dominator_coloring.cpp
+++ src/coloring/dominator_coloring.cpp
@@ -1,4 +1,5 @@
 #include "coloring/dominator_coloring.hpp"
+#include "sat.hpp"
 #include <unordered_set>
 
 bool verify_dominator_coloring(const graph& G,int k, const std::vector<int>& coloring) {
@@ -18,13 +19,23 @@ int choose_next_node(const graph& G,int k,const std::vector<int> coloring) {
 
 std::unordered_set<int> get_available_colors_dominator(const graph& G, int u, int k, const std::vector<int>& coloring) {
     std::vector<bool> colors(k,true);
+    int neighbors_without_color = G.get_neighbors(u).size()+1;
+    int missing_colors = k;
     for(int v:G.get_neighbors(u))
-        if(coloring[v]!=0) colors[coloring[v]-1] = false;
+        if(coloring[v]!=0) {
+            neighbors_without_color--;
+            if(colors[coloring[v]-1]) {
+                colors[coloring[v]-1] = false;
+                missing_colors--;
+            }
+        }
+
+    if(missing_colors>neighbors_without_color) return {};
 
     std::unordered_set<int> available_colors;
     for(int color=1;color<=k;color++)
         if(colors[color-1]) available_colors.insert(color);
-    
+    if(available_colors.empty()) available_colors = {1};
     return available_colors;
 }
 
