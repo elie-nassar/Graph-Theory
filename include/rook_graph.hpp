4513e2f8ccd2607a42c17db88e93da9b9bbebae0
Elie Nassar <nassarelie98@gmail.com>
2026-06-04 11:12:50 +0200

    convert getx and gety rook_graph

--- include/rook_graph.hpp
+++ include/rook_graph.hpp
@@ -11,3 +11,7 @@ class rook_graph : public graph {
         int save(std::string filename, const std::vector<int> &coloring) const;
         static rook_graph random_directed(int n);
 };
+
+int convert(int x,int y,int n);
+int getX(int u,int n);
+int getY(int u,int n);
