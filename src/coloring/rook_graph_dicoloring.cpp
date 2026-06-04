77cab75b1bed4b99077e8a9d7ca3b0ece921c6dd
Elie Nassar <nassarelie98@gmail.com>
2026-06-04 13:36:58 +0200

    Get available colors for c3 cycles is ugraded

--- src/coloring/rook_graph_dicoloring.cpp
+++ src/coloring/rook_graph_dicoloring.cpp
@@ -16,40 +16,64 @@ std::unordered_set<int> get_available_colors_C3(const rook_graph& G, int u, int 
 
     if(x<n-2) {
         int u1 = u;
-        int u2 = convert(x+1,y,n);
-        int u3 = convert(x+2,y,n);
-        if(coloring[u2]!=0 && coloring[u2]==coloring[u3] && colors[coloring[u2]-1] && (is_3_cyclic(G,u1,u2,u3) || is_3_cyclic(G,u3,u2,u1))) colors[coloring[u2]-1] = false;
+        for(int x2=x+1;x2<n-1;x2++) {
+            int u2 = convert(x2,y,n);
+            for(int x3=x2+1;x3<n;x3++) {
+                int u3 = convert(x3,y,n);
+                if(coloring[u2]!=0 && coloring[u2]==coloring[u3] && colors[coloring[u2]-1] && (is_3_cyclic(G,u1,u2,u3) || is_3_cyclic(G,u3,u2,u1))) colors[coloring[u2]-1] = false;
+            }
+        }
     }
     if(x>0 && x<n-1) {
-        int u1 = convert(x-1,y,n);
-        int u2 = u;
-        int u3 = convert(x+1,y,n);
-        if(coloring[u1]!=0 && coloring[u1]==coloring[u3] && colors[coloring[u1]-1] && (is_3_cyclic(G,u1,u2,u3) || is_3_cyclic(G,u3,u2,u1))) colors[coloring[u1]-1] = false;
+        for(int x1=x-1;x1>=0;x1--) {
+            int u1 = convert(x1,y,n);
+            int u2 = u;
+            for(int x3=x+1;x3<n;x3++) {
+                int u3 = convert(x3,y,n);
+                if(coloring[u1]!=0 && coloring[u1]==coloring[u3] && colors[coloring[u1]-1] && (is_3_cyclic(G,u1,u2,u3) || is_3_cyclic(G,u3,u2,u1))) colors[coloring[u1]-1] = false;
+            }
+        }
     }
     if(x>1) {
-        int u1 = convert(x-2,y,n);
-        int u2 = convert(x-1,y,n);
-        int u3 = u;
-        if(coloring[u1]!=0 && coloring[u1]==coloring[u2] && colors[coloring[u1]-1] && (is_3_cyclic(G,u1,u2,u3) || is_3_cyclic(G,u3,u2,u1))) colors[coloring[u1]-1] = false;
+        for(int x1=x-2;x1>=0;x1--) {
+            int u1 = convert(x1,y,n);
+            for(int x2=x-1;x2>x1;x2--) {
+                int u2 = convert(x2,y,n);
+                int u3 = u;
+                if(coloring[u1]!=0 && coloring[u1]==coloring[u2] && colors[coloring[u1]-1] && (is_3_cyclic(G,u1,u2,u3) || is_3_cyclic(G,u3,u2,u1))) colors[coloring[u1]-1] = false;
+            }
+        }
     }
 
     if(y<n-2) {
         int u1 = u;
-        int u2 = convert(x,y+1,n);
-        int u3 = convert(x,y+2,n);
-        if(coloring[u2]!=0 && coloring[u2]==coloring[u3] && colors[coloring[u2]-1] && (is_3_cyclic(G,u1,u2,u3) || is_3_cyclic(G,u3,u2,u1))) colors[coloring[u2]-1] = false;
+        for(int y2=y+1;y2<n-1;y2++) {
+            int u2 = convert(x,y2,n);
+            for(int y3=y2+1;y3<n;y3++) {
+                int u3 = convert(x,y3,n);
+                if(coloring[u2]!=0 && coloring[u2]==coloring[u3] && colors[coloring[u2]-1] && (is_3_cyclic(G,u1,u2,u3) || is_3_cyclic(G,u3,u2,u1))) colors[coloring[u2]-1] = false;
+            }
+        }
     }
     if(y>0 && y<n-1) {
-        int u1 = convert(x,y-1,n);
-        int u2 = u;
-        int u3 = convert(x,y+1,n);
-        if(coloring[u1]!=0 && coloring[u1]==coloring[u3] && colors[coloring[u1]-1] && (is_3_cyclic(G,u1,u2,u3) || is_3_cyclic(G,u3,u2,u1))) colors[coloring[u1]-1] = false;
+        for(int y1=y-1;y1>=0;y1--) {
+            int u1 = convert(x,y1,n);
+            int u2 = u;
+            for(int y3=y+1;y3<n;y3++) {
+                int u3 = convert(x,y3,n);
+                if(coloring[u1]!=0 && coloring[u1]==coloring[u3] && colors[coloring[u1]-1] && (is_3_cyclic(G,u1,u2,u3) || is_3_cyclic(G,u3,u2,u1))) colors[coloring[u1]-1] = false;
+            }
+        }
     }
     if(y>1) {
-        int u1 = convert(x,y-2,n);
-        int u2 = convert(x,y-1,n);
-        int u3 = u;
-        if(coloring[u1]!=0 && coloring[u1]==coloring[u2] && colors[coloring[u1]-1] && (is_3_cyclic(G,u1,u2,u3) || is_3_cyclic(G,u3,u2,u1))) colors[coloring[u1]-1] = false;
+        for(int y1=y-2;y1>=0;y1--) {
+            int u1 = convert(x,y1,n);
+            for(int y2=x-1;y2>y1;y2--) {
+                int u2 = convert(x,y2,n);
+                int u3 = u;
+                if(coloring[u1]!=0 && coloring[u1]==coloring[u2] && colors[coloring[u1]-1] && (is_3_cyclic(G,u1,u2,u3) || is_3_cyclic(G,u3,u2,u1))) colors[coloring[u1]-1] = false;
+            }
+        }
     }
 
     std::unordered_set<int> available_colors;
